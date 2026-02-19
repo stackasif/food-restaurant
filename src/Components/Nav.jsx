0229d78393854fe995bcc67d44a4b5db6db7ba47
stackasif <iamasif11996@gmail.com>
2026-02-19 13:22:50 +0530

    updated nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,23 +5,24 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Nav =() =>{
     return(
-        <div className="w-full h-25 flex justify-between items-center px-8">
+        <div className="w-full h-20 md:h-25 flex justify-between items-center px-4 md:px-8">
 
-            <div className="w-15 h-15 bg-white flex justify-center items-center rounded-md shadow-xl">
-                <IoFastFoodOutline className="text-blue-500 w-7.5 h-7.5 "/>
+            <div className="w-10 md:w-15 h-10 md:h-15 bg-white flex justify-center items-center rounded-md shadow-xl">
+                <IoFastFoodOutline className="text-blue-500 w-5 md:w-7.5 h-5 md:h-7.5 "/>
             </div>
 
-            <form action="" className="flex items-center gap-2 px-4 bg-white w-[70%] h-[60%] rounded-md shadow-md">
+            <form action="" className="flex items-center gap-2 px-4 bg-white w-[65%] md:w-[70%] h-[40%] md:h-[60%] rounded-md shadow-md">
                 <IoSearch className="w-5 h-5 text-blue-500"/>
-                <input type="text" placeholder="Search Items...." className="w-full outline-none"/>
+                <input type="text" placeholder="Search Items...." className="w-full outline-none text-[12px] md:text-[16px]"/>
             </form>
 
-             <div className="w-15 h-15 bg-white flex justify-center items-center rounded-md shadow-xl relative ">
-                <span className="absolute top-0 right-2 text-blue-500 font-semibold">
+             <div className="w-10 md:w-15 h-10 md:h-15 bg-white flex justify-center items-center rounded-md shadow-xl relative ">
+                <span className="absolute top-0 right-1 md:right-2 text-blue-500 font-semibold text-[12px] md:text-[16px]">
                     0
                 </span>
-                <MdOutlineShoppingCart className="text-blue-500  w-7.5 h-7.5"/>
+                <MdOutlineShoppingCart className="text-blue-500 w-5 md:w-7.5 h-5 md:h-7.5"/>
             </div>
+
         </div>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,15 +6,12 @@ const Home=()=>{
     return (
         <div className="bg-blue-100 w-full min-h-screen">
             <Nav />
-            <div className="w-16 h-16 flex gap-5">
+            <div className=" flex flex-wrap gap-5 justify-center items-center w-[full] m-2 md:m-4">
                 {
                     category.map((elem)=>(
-                        <div key={elem.id} className=" bg-amber-400">
-                            {/* {elem.image} */}
-                            <h2>
-                                {elem.name}
-                            </h2>
-                          <img src={elem.image} alt="" />
+                        <div key={elem.id} className="bg-white text-gray-700 w-35 h-35 flex flex-col justify-center items-center p-3 cursor-pointer gap-3 rounded-lg shadow-lg transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:bg-blue-100 ">
+                            <img src={elem.image} alt="" className="w-15 h-15" />
+                             <h2 className="font-bold">{elem.name}</h2>
                         </div>
                     ))
                 }
