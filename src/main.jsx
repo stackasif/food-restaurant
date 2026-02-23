4328cc749dd1f19f40e0efb584b215757a728d22
stackasif <iamasif11996@gmail.com>
2026-02-24 02:13:31 +0530

    fixed case issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import UserContext from './context/userContext.jsx'
+import UserContext from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <UserContext>
