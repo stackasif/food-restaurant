e8768d3000fe98ac6f7ad47381256c1501e4a573
stackasif <iamasif11996@gmail.com>
2026-02-24 02:19:41 +0530

    fixed import case sensitive issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import UserContext from '../src/context/UserContext.jsx'
+import UserContext from "./context/UserContext"
 
 createRoot(document.getElementById('root')).render(
   <UserContext>
